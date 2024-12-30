f4160183ac1464b2c69ac1b8b08eed426ee29b8d
Prithvi V <P.Lnu001@umb.edu>
2024-12-30 15:39:23 -0800

    Second Version

--- src/sections/Footer/Footer.jsx
+++ src/sections/Footer/Footer.jsx
@@ -1,13 +1,28 @@
-import styles from './FooterStyles.module.css'
+import styles from './FooterStyles.module.css';
 
 function Footer() {
   return (
-    <section id='footer' className={styles.container}>
-      <p>&copy; 2024 Prithvi Vasanth Kumar.<br/>
-      All rights reserved.
+    <footer id='footer' className={styles.container}>
+      <div className={styles.footerSection}>
+        <h2>Get in Touch</h2>
+        <div className={styles.contactDetails}>
+          <div className={styles.contactItem}>
+            <h3>Location</h3>
+            <p>United States</p>
+          </div>
+          <div className={styles.contactItem}>
+            <h3>Contact Directly</h3>
+            <p>Email: <a href="mailto:prithvivasanth15@gmail.com">prithvivasanth15@gmail.com</a></p>
+            <p>Phone: +1 (857)-693-8739</p>
+          </div>
+        </div>
+      </div>
+      <hr/>
+      <p>&copy; 2024 Prithvi Vasanth Kumar.<br />
+        All rights reserved.
       </p>
-    </section>
-  )
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -8,6 +8,7 @@ import chatbot from '../../assets/chatbot.png'
 import travel from '../../assets/travel.png'
 import product from '../../assets/product.png'
 import ride from '../../assets/ride.png'
+import scraper from '../../assets/scraper.png'
 
 function Projects() {
   return (
@@ -15,44 +16,44 @@ function Projects() {
       <h1 className="sectionTitle">Projects</h1>
       <div className={styles.projectsContainer}>
         <ProjectCard src={fitness} link='https://github.com/PrithviVK/MultiAgent-Fitness-Application'
-          h3='Fitness Application'
-          p='Application'
+          h3='Fitness'
+          p='Multiagent Application'
           // p='A multi-agent fitness application that allows users to create and join fitness challenges, track their progress, and compete with friends.'
         />
         <ProjectCard src={mnist} link='https://github.com/PrithviVK/Image-Classification'
           h3='Image Classification'
-          p= 'Application'
+          p= 'Fashion MNIST'
           // p='A machine learning project that developed and optimized models for the Fashion MNIST dataset.'
         />
         <ProjectCard src={natops} link='https://github.com/PrithviVK/Human-Activity-Recognition'
           h3='Human Activity Recognition'
-          p= 'Application'
+          p= 'NATOPS Dataset'
           // p='A myriad of machine learning techniques to classify various action classes from the NATOPS dataset'
         />
         <ProjectCard src={covid} link='https://github.com/PrithviVK/Covid-19-Dashboard'
-          h3='Covid-19 Dashboard'
-          p='Application'
+          h3='Covid-19 Prediction'
+          p='Dashboard'
           // p='A dashboard that displays the number of predicted and current cases, deaths, and recoveries for each country.'
         />
         <ProjectCard src={chatbot} link='https://github.com/PrithviVK/Chatbot'
           h3='Chatbot'
-          p='Application'
-          // p='A dashboard that displays the number of predicted and current cases, deaths, and recoveries for each country.'
+          p='Rule-Based Chatbot'
         />
         <ProjectCard src={travel} link='https://github.com/PrithviVK/Web-Tech-Project--Nammoora-Payana'
           h3='Explore Karnataka'
-          p='Application'
-          // p='A dashboard that displays the number of predicted and current cases, deaths, and recoveries for each country.'
+          p='Karnataka Tourism'
         />
         <ProjectCard src={product} link='https://github.com/PrithviVK/E-commerce-Product-Management-System'
           h3='Product Management System'
-          p='Application'
-          // p='A dashboard that displays the number of predicted and current cases, deaths, and recoveries for each country.'
+          p='E-commerce'
         />
         <ProjectCard src={ride} link='https://github.com/PrithviVK/Cloud_Computing'
           h3='Rideshare'
           p='Application'
-          // p='A dashboard that displays the number of predicted and current cases, deaths, and recoveries for each country.'
+        />
+        <ProjectCard src={scraper} link='https://github.com/PrithviVK/Web-Scraper'
+          h3='Webscraper'
+          p='Largest US companies by revenue'
         />
       </div>
     </section>
